8ff2bfcfd2ac5b2f5fdd34822ad0f39b7dbc3280
Julian Avalos <javalos916@hotmail.com>
2015-05-14 13:18:49 -0700

    deleted some lines of code

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 	<!doctype html>
-	<?php
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
-
-$app->run();
-
-?>
 	<html>
 	<head>
 	<meta charset="UTF-8">
