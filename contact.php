153d1717fac14d113a81b24321bed75d7ff7afeb
Julian Avalos <javalos916@hotmail.com>
2015-05-19 15:56:21 -0700

    another edit

--- contact.php
+++ contact.php
@@ -50,34 +50,34 @@
                         <legend class="text-center header">Contact us</legend>
 
                         <label for="name" class="col-sm-2 control-label">Name</label>
-        <div class="col-sm-10">
-            <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
-            <?php echo "<p class='text-danger'>$errName</p>";?>
-        </div>
+    		<div class="col-md-12">
+            	<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+            	<?php echo "<p class='text-danger'>$errName</p>";?>
+        	</div>
    
-    <div class="form-group">
-        <label for="email" class="col-sm-2 control-label">Email</label>
-        <div class="col-sm-10">
+    		<div class="form-group">
+        				<label for="email" class="col-sm-2 control-label">Email</label>
+        <div class="col-me-12">
             <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
             <?php echo "<p class='text-danger'>$errEmail</p>";?>
         </div>
     </div>
     <div class="form-group">
         <label for="message" class="col-sm-2 control-label">Message</label>
-        <div class="col-sm-10">
+        <div class="col-md-12">
             <textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
             <?php echo "<p class='text-danger'>$errMessage</p>";?>
         </div>
     </div>
     <div class="form-group">
         <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
-        <div class="col-sm-10">
+        <div class="col-md-12">
             <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
             <?php echo "<p class='text-danger'>$errHuman</p>";?>
         </div>
     </div>
     <div class="form-group">
-        <div class="col-sm-10 col-sm-offset-2">
+        <div class="col-md-12 col-sm-offset-2">
             <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
         </div>
     </div>
