40c94ffd6979af6744942c8455ababba6178baf4
Julian Avalos <javalos916@hotmail.com>
2015-05-22 10:43:21 -0700

    edited contact_form.php

--- contact_form.php
+++ contact_form.php
@@ -14,7 +14,7 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'javalos916@hotmail.com'; // put your email
+$to = '41a1b069b0da530f435a@cloudmailin.net'; // put your email
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
