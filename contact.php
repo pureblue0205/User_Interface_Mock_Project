2a4ce316bf38fc8670b0944f996c0cd2b61752a6
Julian Avalos <javalos916@hotmail.com>
2015-05-26 10:44:17 -0700

    update

--- contact.php
+++ contact.php
@@ -124,6 +124,8 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
         </div>
     </div>
 </section>   
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
   </body>
