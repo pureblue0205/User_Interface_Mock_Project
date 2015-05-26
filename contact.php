aa3730e12610b6b9fb2e554ce4f3e1c53ef3ceee
Julian Avalos <javalos916@hotmail.com>
2015-05-26 13:13:41 -0700

    added files

--- contact.php
+++ contact.php
@@ -20,6 +20,23 @@
     <meta name="keywords" content="Photography, Cycling, Bycicles, Roadbike, Washington">
     <style type="text/css">
 
+    $(document).ready(function () {
+    $("input#submit").click(function(){
+        $.ajax({
+            type: "POST",
+            url: "process.php", //process to mail
+            data: $('form.contact').serialize(),
+            success: function(msg){
+                $("#thanks").html(msg) //hide button and show thank you
+                $("#form-content").modal('hide'); //hide popup  
+            },
+            error: function(){
+                alert("failure");
+            }
+        });
+    });
+});
+
     </style>
     </head>
 
@@ -47,6 +64,28 @@
         </div>
       </div>
     </nav><!--END OF NAVBAR-->
+
+    <div id="form-content" class="modal hide fade in" style="display: none;">
+    <div class="modal-header">
+        <a class="close" data-dismiss="modal">×</a>
+        <h3>Send me a message</h3>
+    </div>
+    <div class="modal-body">
+        <form class="contact" name="contact">
+            <label class="label" for="name">Your Name</label><br>
+            <input type="text" name="name" class="input-xlarge"><br>
+            <label class="label" for="email">Your E-mail</label><br>
+            <input type="email" name="email" class="input-xlarge"><br>
+            <label class="label" for="message">Enter a Message</label><br>
+            <textarea name="message" class="input-xlarge"></textarea>
+        </form>
+    </div>
+    <div class="modal-footer">
+        <input class="btn btn-success" type="submit" value="Send!" id="submit">
+        <a href="#" class="btn" data-dismiss="modal">Nah.</a>
+    </div>
+</div>
+<div id="thanks"><p><a data-toggle="modal" href="#form-content" class="btn btn-primary btn-large">Modal powers, activate!</a></p></div>
             
     </script> 
     <footer id="foot01"></footer>
