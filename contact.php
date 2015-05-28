a876cd0af4014240cc2667704e49c33e0367594e
Julian Avalos <javalos916@hotmail.com>
2015-05-28 10:04:44 -0700

    replaced contact

--- contact.php
+++ contact.php
@@ -1,48 +1,57 @@
-    <!doctype html>
+<?php
+    if ($_POST["submit"]) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = $_POST[$name]; 
+        $to = 'javalos916@hotmail.com'; 
+        $subject = 'Message from Contact Demo ';
+        
+        $body ="From: $name\n E-Mail: $email\n Message:\n $message";
+        // Check if name has been entered
+        if (!$_POST['name']) {
+            $errName = 'Please enter your name';
+        }
+        
+        // Check if email has been entered and is valid
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+        
+        //Check if message has been entered
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        if ($human !== 5) {
+            $errHuman = 'Your anti-spam is incorrect';
+        }
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+    if (mail ($to, $subject, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+    }
+}
+    }
+?>
 
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <title>Cole Heilborn</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/agency.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
-    <link type="text/css" href="site.css" rel="stylesheet">
-
-
-    <meta name="description" content="Sharing My Love For Cycling">
-    <meta name="keywords" content="Photography, Cycling, Bycicles, Roadbike, Washington">
-    <style type="text/css">
-
-    $(document).ready(function () {
-    $("input#submit").click(function(){
-        $.ajax({
-            type: "POST",
-            url: "process.php", //process to mail
-            data: $('form.contact').serialize(),
-            success: function(msg){
-                $("#thanks").html(msg) //hide button and show thank you
-                $("#form-content").modal('hide'); //hide popup  
-            },
-            error: function(){
-                alert("failure");
-            }
-        });
-    });
-});
-
-    </style>
-    </head>
-
-    <body>
-
-    <!--START OF NAVBAR-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bootstrap contact form with PHP example by BootstrapBay.com.">
+    <meta name="author" content="BootstrapBay.com">
+    <title>Bootstrap Contact Form With PHP Example</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  </head>
+  <body>
+  <!--START OF NAVBAR-->
+  <!--LETS SEE HOW THIS WORKS-->
      <nav class="navbar navbar-inverse">
       <div class="container-fluid">
         <div class="navbar-header">
@@ -58,43 +67,67 @@
               </ul>
             </li>-->
             <li><a href="photo.php">Gallery</a></li>
-            <li><a href="about.php">About</a></li>
-            <li class="active"><a href="contact.php">Contact</a></li>
+            <li class = "active"><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
           </ul>
         </div>
       </div>
-    </nav><!--END OF NAVBAR-->
+    </nav><!--end of navbar-->
+    <section id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Contact Us</h2>
 
-    <div class="container text-center">
-    <div class="well well-large">
-        <h2>Contact Us</h2>
-        <div id="form-content" class="modal hide fade in" style="display: none;">
-            <div class="modal-header">
-                <a class="close" data-dismiss="modal"></a>
-                <h3></h3>
-            </div>
-            <div class="modal-body">
-                <form class="contact" name="contact">
-                    <label class="label" for="name">Your Name</label><br>
-                    <input type="text" name="name" class="input-xlarge"><br>
-                    <label class="label" for="email">Your E-mail</label><br>
-                    <input type="email" name="email" class="input-xlarge"><br>
-                    <label class="label" for="message">Enter a Message</label><br>
-                    <textarea name="message" class="input-xlarge"></textarea>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <input class="btn btn-success" type="submit" value="Send!" id="submit">
-                <a href="#" class="btn" data-dismiss="modal">Cancel</a>
+                <h3 class="section-subheading text-muted">If you have any questions or ideas contact us below</h3>
             </div>
         </div>
-        <div id="thanks"><p><a data-toggle="modal" href="#form-content" class="btn btn-primary btn-large">Contact</a></p></div>
+        <div class="row">
+            <div class="col-lg-12">
+                <form name="sentMessage" id="contactForm" novalidate>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your Name *" id="name" required
+                                       data-validation-required-message="Please enter your name." name="contact[name]">
+
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="Your Email *" id="email" required
+                                       data-validation-required-message="Please enter your email address."  name="contact[email]">
+
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input type="tel" class="form-control" placeholder="Your Phone *" id="phone" required
+                                       data-validation-required-message="Please enter your phone number."  name="contact[phone]">
+
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <textarea class="form-control" placeholder="Your Message *" id="message" required
+                                          data-validation-required-message="Please enter a message."  name="contact[message]"></textarea>
+
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                        <div class="col-lg-12 text-center">
+                            <div id="success"></div>
+                            <input type="submit" class="btn btn-xl" value="Send Message"></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-</div>
-            
-    </script> 
-    <footer id="foot01"></footer>
-    <script type="text.javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script type="text/javascript" src="./script.js"></script>
-    </body>
-    </html>
+</section>   
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
