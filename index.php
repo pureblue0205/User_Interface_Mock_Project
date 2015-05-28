ebd6370206e8fc8aaa7aac806e5794c022ce6eec
Julian Avalos <javalos916@hotmail.com>
2015-05-28 09:33:18 -0700

    update

--- index.php
+++ index.php
@@ -22,22 +22,6 @@
 
 	</style>
 
-	$(document).ready(function () {
-    $("input#submit").click(function(){
-        $.ajax({
-            type: "POST",
-            url: "process.php", //process to mail
-            data: $('form.contact').serialize(),
-            success: function(msg){
-                $("#thanks").html(msg) //hide button and show thank you
-                $("#form-content").modal('hide'); //hide popup  
-            },
-            error: function(){
-                alert("failure");
-            }
-        });
-    });
-});
 	</head>
 
 	<body>
